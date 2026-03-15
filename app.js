b951f78fd7f3b6abf703adc3fa64c38680421195
sisshmue <sisshmue404@gmail.com>
2026-03-15 22:18:55 +0700

    add express-rate-limit middleware to enhance API request handling and prevent abuse

--- app.js
+++ app.js
@@ -5,13 +5,20 @@ import express from "express";
 import analyzeRouter from './src/routes/analysisRoutes.js'
 import swaggerSpec from './src/config/swagger.js';
 import swaggerUi from 'swagger-ui-express'
+import rateLimit from 'express-rate-limit';
 
 const app = express();
 
 app.use(express.json());
-
-
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 *1000,
+    max : 5,
+    message : 'Too many requests, please try again later.'
+})
+
+app.use(limiter);
 
 app.use('/api/analyze', analyzeRouter);
 
