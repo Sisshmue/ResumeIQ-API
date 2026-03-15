12f68dc1a7a3df8b6809c4f9751b553f9530befc
sisshmue <sisshmue404@gmail.com>
2026-03-15 15:37:09 +0700

    adjust to get clean text

--- src/services/resumeParser.js
+++ src/services/resumeParser.js
@@ -9,7 +9,9 @@ export const extractResumeText = async (filePath) => {
         const dataBuffer = fs.readFileSync(filePath);
         const parser = new PDFParse({data: dataBuffer});
         const data = await parser.getText();
-        return data.text;
+
+        const cleanText = data.text.replace(/\s+/g, ' ').trim();
+        return cleanText;
     } catch (error) {
         throw new Error('Error extracting resume text: ' + error.message);
     }
