5c09b0c337007abcb6cce95520320250cb46c786
sisshmue <sisshmue404@gmail.com>
2026-03-20 15:04:42 +0700

    add console log for DATABASE_URL in prisma configuration

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -4,6 +4,9 @@ dotenv.config();
 import {PrismaClient} from '../generated/prisma/client.ts';
 import {PrismaPg} from "@prisma/adapter-pg";
 
+console.log(process.env.DATABASE_URL);
+
+
 const adapter = new PrismaPg({
     connectionString : process.env.DATABASE_URL
 })
