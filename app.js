682b934e85c8ec9538a97eb08031c52e0f9afa8d
sisshmue <sisshmue404@gmail.com>
2026-03-23 11:00:33 +0700

    feat: add CORS support and update package dependencies

--- app.js
+++ app.js
@@ -8,13 +8,16 @@ import swaggerUi from "swagger-ui-express";
 import fs from "fs";
 import { limiter } from "./src/middlewares/limitMiddleware.js";
 import authRouter from "./src/routes/authRoutes.js";
-import historyRouter from "./src/routes/historyRoutes.js"
+import historyRouter from "./src/routes/historyRoutes.js";
+import cors from "cors";
 
 const app = express();
 
 if (!fs.existsSync("uploads")) {
   fs.mkdirSync("uploads");
 }
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -24,7 +27,7 @@ app.use("/api/analyze", limiter, analyzeRouter);
 
 app.use("/api/auth", authRouter);
 
-app.use('/api/history', historyRouter);
+app.use("/api/history", historyRouter);
 
 const PORT = 3000;
 app.listen(PORT, () => {
